31ae22b959f172d7d25ccf161542572d62be83c9
gvillate <gvillate@asu.edu>
2020-02-16 16:44:11 -0700

    Adding Xcode Project

---
+++ animalApp/animalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animalApp
+//
+//  Created by Gabriela Villate on 2/16/20.
+//  Copyright © 2020 Gabriela Villate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
