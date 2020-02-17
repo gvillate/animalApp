27c607cc6c2bdee0b86335ee932fe9c632dbc9d6
Alexandra Plukis <aplukis@asu.edu>
2020-02-16 17:08:42 -0700

    entire project

--- animalApp/animalApp/ViewController.swift
+++ animalApp/animalApp/ViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var animalList: AnimalList?
-
+    var animalList: AnimalList = AnimalList()
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var dietControl: UISegmentedControl!
+    @IBOutlet weak var soundField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        animalList = AnimalList()
-        
     }
     
+    
+    @IBAction func add(_ sender: Any) {
+        if let name = nameField.text, let sound = soundField.text {
+            
+        }
+    }
     
 
 
